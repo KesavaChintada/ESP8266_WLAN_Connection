b3d16f2df630b5696762251174a4acc7e425fc71
KesavaChintada <chintadakesava@gmail.com>
2020-09-29 23:09:18 +0530

    Prototype

---
+++ wifi.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h>
+#include <string.h>
+const char* ssid = "Airtel";
+const char* pass = "9247116180";
+char Data;
+
+WiFiServer server(65432);
+WiFiClient clint;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid,pass);
+  Serial.println("connecting to wifi");
+  while(WiFi.status()!= WL_CONNECTED){delay(500);}
+  Serial.println("connected sucessfully");
+  
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.macAddress());
+  
+  server.begin();
+}
+
+void ReadData(){
+    while(clint.available()){
+      Data = clint.read();
+      Serial.println(Data);
+    }
+    clint.println("recieved!");   
+}
+
+void loop() {
+  
+  clint = server.available();
+  
+  if(clint){
+    if(clint.connected()){
+      Serial.println("Clint is connected!");
+      ReadData();
+    }
+    else{clint.stop();}
+  }
+ delay(500);
+}
